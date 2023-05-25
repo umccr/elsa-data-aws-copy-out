5232aabd2a7e648c613bb489de236d9e6266f16f
andrewpatto <andrew@patto.net>
2023-05-25 10:26:20 +1000

    Tiny fixes for demo

--- deploy/manual/stacks.ts
+++ deploy/manual/stacks.ts
@@ -4,36 +4,60 @@ import { App } from "aws-cdk-lib";
 
 const app = new App();
 
-const localDevTestId = "ElsaDataLocalDevTestCopyOutStack";
+{
+  const localDevTestId = "ElsaDataLocalDevTestCopyOutStack";
 
-new CopyOutStack(app, localDevTestId, {
-  // the stack can only be deployed to 'dev'
-  env: {
-    account: "843407916570",
-    region: "ap-southeast-2",
-  },
-  tags: {
-    "umccr-org:Product": "ElsaData",
-    "umccr-org:Stack": localDevTestId,
-  },
-  isDevelopment: true,
-  infrastructureStackName: "ElsaDataLocalDevTestInfrastructureStack",
-  infrastructureSubnetSelection: SubnetType.PRIVATE_WITH_EGRESS,
-});
+  new CopyOutStack(app, localDevTestId, {
+    // the stack can only be deployed to 'dev'
+    env: {
+      account: "843407916570",
+      region: "ap-southeast-2",
+    },
+    tags: {
+      "umccr-org:Product": "ElsaData",
+      "umccr-org:Stack": localDevTestId,
+    },
+    isDevelopment: true,
+    infrastructureStackName: "ElsaDataLocalDevTestInfrastructureStack",
+    infrastructureSubnetSelection: SubnetType.PRIVATE_WITH_EGRESS,
+  });
+}
 
-const agId = "ElsaDataAgCopyOutStack";
+{
+  const agDemoId = "ElsaDataDemoAustralianGenomicsCopyOutStack";
 
-new CopyOutStack(app, agId, {
-  // the stack can only be deployed to 'ag'
-  env: {
-    account: "602836945884",
-    region: "ap-southeast-2",
-  },
-  tags: {
-    "umccr-org:Product": "ElsaData",
-    "umccr-org:Stack": agId,
-  },
-  isDevelopment: false,
-  infrastructureStackName: "ElsaDataAustralianGenomicsInfrastructureStack",
-  infrastructureSubnetSelection: SubnetType.PRIVATE_ISOLATED,
-});
+  new CopyOutStack(app, agDemoId, {
+    // the stack can only be deployed to 'ag'
+    env: {
+      account: "602836945884",
+      region: "ap-southeast-2",
+    },
+    tags: {
+      "umccr-org:Product": "ElsaData",
+      "umccr-org:Stack": agDemoId,
+    },
+    isDevelopment: false,
+    infrastructureStackName:
+      "ElsaDataDemoAustralianGenomicsInfrastructureStack",
+    infrastructureSubnetSelection: SubnetType.PRIVATE_ISOLATED,
+  });
+}
+
+{
+  const agId = "ElsaDataAustralianGenomicsCopyOutStack";
+
+  new CopyOutStack(app, agId, {
+    // the stack can only be deployed to 'ag'
+    env: {
+      account: "602836945884",
+      region: "ap-southeast-2",
+    },
+    tags: {
+      "umccr-org:Product": "ElsaData",
+      "umccr-org:Stack": agId,
+    },
+    isDevelopment: false,
+    infrastructureStackName: "ElsaDataAustralianGenomicsInfrastructureStack",
+    infrastructureSubnetSelection: SubnetType.PRIVATE_ISOLATED,
+  });
+}
